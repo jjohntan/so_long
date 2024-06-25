5d0529fbf85a29ce3ddc8d71850d549f2bb55eb2
Jek John Tan <jetan@u82z05s01.42kl.edu.my>
2024-06-25 11:31:13 +0800

    update

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 16:29:15 by jetan             #+#    #+#             */
-/*   Updated: 2024/06/25 09:35:32 by jetan            ###   ########.fr       */
+/*   Updated: 2024/06/25 11:30:44 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ void	check_ber(char **av)
 	}
 }
 
-char	read_map(t_data *game, char *av)
+char	read_map(char *av)
 {
 	int		fd;
 	char	*read_map;
 	char	*load_map;
+	char	**map;
 	
 	load_map = ft_strdup("");
 	fd = open(av, O_RDONLY);
@@ -42,7 +43,11 @@ char	read_map(t_data *game, char *av)
 		read_map = get_next_line(fd);
 		if (!read)
 			break;
-		load_map = ft_strjoin(load_map, read_map);
 		free(read_map);
+		load_map = ft_strjoin(load_map, read_map);
 	}
+	map = ft_split(load_map, "\n");
+	free(read_map);
+	close(fd);
+	return (map);
 }
