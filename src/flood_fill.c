076ad5d503b8b0cd862ac822c0b72bce7a9fa829
Jek John Tan <jetan@u90z05s02.42kl.edu.my>
2024-08-20 19:08:34 +0800

    update

--- src/flood_fill.c
+++ src/flood_fill.c
@@ -6,18 +6,19 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 13:45:23 by jetan             #+#    #+#             */
-/*   Updated: 2024/08/20 14:01:42 by jetan            ###   ########.fr       */
+/*   Updated: 2024/08/20 18:01:30 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-int	flood_fill(t_data *data, int x, int y)
+void	flood_fill(t_data *data, int x, int y)
 {
-	if ()
+	if (x < 0 || x >= data->width || y < 0 || y >= data->height)
 	data->map[y][x] = 'V';
 	flood_fill(data, x - 1, y);
 	flood_fill(data, x + 1, y);
 	flood_fill(data, x, y - 1);
 	flood_fill(data, x, y + 1);
 }
+
