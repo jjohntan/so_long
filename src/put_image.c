0466324bdf278b726c8a1c0e1d44567cdec66d94
Jek John Tan <jetan@u90z05s02.42kl.edu.my>
2024-08-21 15:31:15 +0800

    add put_img_to_win

--- src/put_image.c
+++ src/put_image.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 14:50:15 by jetan             #+#    #+#             */
-/*   Updated: 2024/08/21 15:15:43 by jetan            ###   ########.fr       */
+/*   Updated: 2024/08/21 15:29:45 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,34 @@ void	put_img_to_char(t_data *data)
 	data->player = mlx_xpm_file_to_image(data->mlx_ptr, , img_width, img_height);
 }
 
-void	put_img_to_win(t_data *data, int y, int x)
+void	check_and_put_img(t_data *data, int y, int x)
 {
 	if (data->map[y][x] == '0')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->empty, XPM_WIDTH, XPM_HEIGHT);
-	if (data->map[y][x] == '1')
+	else if (data->map[y][x] == '1')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->empty, XPM_WIDTH, XPM_HEIGHT);
-	if (data->map[y][x] == 'C')
+	else if (data->map[y][x] == 'C')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->empty, XPM_WIDTH, XPM_HEIGHT);
-	if (data->map[y][x] == 'E')
+	else if (data->map[y][x] == 'E')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->empty, XPM_WIDTH, XPM_HEIGHT);
-	if (data->map[y][x] == 'P')
+	else if (data->map[y][x] == 'P')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->empty, XPM_WIDTH, XPM_HEIGHT);
+}
+
+void	put_img_to_win(t_data *data)
+{
+	int	y;
+	int	x;
+	
+	y = 0;
+	while (data->map[y])
+	{
+		x = 0;
+		while (data->map[y][x])
+		{
+			check_and_put_img(data, y, x);
+			x++;	
+		}
+		y++;
+	}
 }
